f0912f63e46cc3bda7900269f53be877374c8397
Danbesy <danya.danbesy@gmail.com>
2026-02-25 20:35:53 +0300

    add checkServer function

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net"
 	"os"
+	"time"
 )
 
 type Server struct {
@@ -30,6 +32,18 @@ func loadServers(filename string) ([]Server, error) {
 	return servers, nil
 }
 
+func checkServer(s Server) bool {
+	address := net.JoinHostPort(s.IP, fmt.Sprintf("%d", s.Port))
+
+	conn, err := net.DialTimeout("tcp", address, 3*time.Second)
+	if err != nil {
+		return false
+	}
+
+	conn.Close()
+	return true
+}
+
 func main() {
 	fmt.Println("Healthcheck script has started!")
 
@@ -43,5 +57,14 @@ func main() {
 
 	for _, server := range servers {
 		fmt.Println("Имя сервера:", server.Name, "Айпи сервера:", server.IP, "Порт сервера:", server.Port)
+	}
+
+	fmt.Println("Проверка доступности серверов:")
+	for _, server := range servers {
+		if checkServer(server) {
+			fmt.Println("OK", server.Name, "доступен")
+		} else {
+			fmt.Println("FAIL", server.Name, "недоступен")
+		}
 	}
 }
